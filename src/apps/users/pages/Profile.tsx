166168f0ea9fcb6f9d1392c6137d0a0e9b67f653
samuel aniekan <samuelaniekan680@gmail.com>
2022-09-16 19:41:03 +0100

    modified profile page

--- src/apps/users/pages/Profile.tsx
+++ src/apps/users/pages/Profile.tsx
@@ -18,6 +18,32 @@ const Profile = () => {
         <h5 className="font-size-24 mb-1 text-truncate">John bobs</h5>
         <p className="text- text-truncate mb-1 text-success">online</p>
       </div>
+      <div className="p-4 user-profile-desc">
+        <div className="text-muted">
+          <p className="mb-4 text-muted font-15">
+            If several languages coalesce, the grammar of the resulting language
+            is more simple and regular than that of the individual.
+          </p>
+        </div>
+        <div className="about-box">
+          <div className="about-item">
+            <p className="font-size-15 text-muted mb-1"> Name </p>
+            <h5 className="font-size-14"> Patricia Smith </h5>
+          </div>
+          <div className="about-item">
+            <p className="font-size-15 text-muted mb-1"> Email </p>
+            <h5 className="font-size-14"> Patricia@Smith </h5>
+          </div>
+          <div className="about-item">
+            <p className="font-size-15 text-muted mb-1"> Location </p>
+            <h5 className="font-size-14"> Patricia@Smith </h5>
+          </div>
+          <div className="about-item">
+            <p className="font-size-15 text-muted mb-1"> NO of friends </p>
+            <h5 className="font-size-14"> 10 </h5>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
